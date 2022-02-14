1cf89f653ca9939e062fb2a8fa9fbcd974514570
Aditya Mathur <89768498+adii9@users.noreply.github.com>
2022-02-15 01:08:00 +0530

    Added static files, templates.

--- PF/views.py
+++ PF/views.py
@@ -1,7 +1,8 @@
+from unicodedata import name
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
     if 'GET':
-        return HttpResponse('Hello World!!')
+        return render(request, 'index.html')
