997eb729d15ae41d5e70510ba97dcf8e92cf2a0c
Aditya Mathur <89768498+adii9@users.noreply.github.com>
2022-02-19 13:11:32 +0530

    Add typewriter effect on html page and added database name in settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'My_Portfolio_Django',
+        'USER': 'postgres',
+        'PASSWORD': 'Himachal260117@',
+        'HOST': 'localhost'
     }
 }
 
